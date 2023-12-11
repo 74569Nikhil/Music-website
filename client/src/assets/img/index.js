4ad2a844356ba60166a984621c89813a85d51f88
Nikhil Badhe <nikhilbadhe1591999@gmail.com>
2023-12-11 11:02:02 +0530

    new

---
+++ client/src/assets/img/index.js
@@ -0,0 +1,4 @@
+export { default as Logo } from "./logo.png";
+export { default as Nikhil  } from "./Nikhil.png";
+export { default as Ved } from "./Ved.jpg";
+export { default as Shishir } from "./Shishir.jpg";
